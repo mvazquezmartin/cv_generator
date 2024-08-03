73180d0ec2bde265c85edb74147a0e776954dcb5
mvazquez <matias.vazquezmartin@gmail.com>
2024-08-02 22:09:18 -0300

    style: minor changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,37 @@
 import { LanguageHandler } from '../LanguageHandler/LanguageHandler';
 import { SVG_downloadIcon } from '@/assets/svg';
+import { useTranslation } from 'react-i18next';
+import { PDFDocument } from '@/pages/CreateCV/CVDesign';
 import './header.css';
+import { PDFDownloadLink } from '@react-pdf/renderer';
 
-export const Header = ({ showBtn }) => {
+export const Header = ({ showBtn, name, contact, experiences }) => {
+  const { t } = useTranslation();
   return (
     <header>
       <div className="header-flex">
-        <div className="logo-header"></div>
+        <div className="logo-header">
+          <a href="/">INICIO</a>
+        </div>
         <div className="language-download">
           {showBtn ? (
             <LanguageHandler />
           ) : (
-            <button>
-              <SVG_downloadIcon />
-            </button>
+            <PDFDownloadLink
+              document={
+                <PDFDocument
+                  name={name}
+                  contact={contact}
+                  experiences={experiences}
+                />
+              }
+              fileName={`cv_${name.split(' ').join('_')}.pdf`}
+            >
+              <button className="btn-download">
+                <SVG_downloadIcon />
+                {t('header.btnDownload')}
+              </button>
+            </PDFDownloadLink>
           )}
         </div>
       </div>
