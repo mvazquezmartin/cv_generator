35b42c84dcb8a13627a26cb742347de8fec77e0b
mvazquez <matias.vazquezmartin@gmail.com>
2024-08-02 22:10:38 -0300

    feat: add english t

--- src/pages/Home/MainLayout.jsx
+++ src/pages/Home/MainLayout.jsx
@@ -1,21 +1,34 @@
 import { useTranslation } from 'react-i18next';
-import { Footer } from '../../components/Footer/Footer';
-import { Header } from '../../components/Header/Header';
+import { Footer } from '@/components/Footer/Footer';
+import { Header } from '@/components/Header/Header';
+import { useNavigate } from 'react-router-dom';
 import './mainLayout.css';
 
-export const MainLayout = ({ showBtn }) => {
+export const MainLayout = () => {
+  const navigate = useNavigate();
+
   const { t } = useTranslation();
+
+  const handleNavigate = () => {
+    navigate('/editorcv');
+  };
+
   return (
     <>
-      <Header showBtn={showBtn} />
+      <Header showBtn={true} />
       <div className="main-container">
         <main>
           <div className="home-title">
-            <h1>{t('home.title')}</h1>
-            <h2>{t('home.description')}</h2>
+            <h1>
+              <span>{t('home.title.first')}</span>
+              <span>{t('home.title.second')}</span>
+            </h1>
+            <p>{t('home.description')}</p>
           </div>
           <div className="home-action-btn">
-            <button className="btn-start">{t('home.button')}</button>
+            <button className="btn-start" onClick={handleNavigate}>
+              {t('home.button')}
+            </button>
           </div>
         </main>
       </div>
