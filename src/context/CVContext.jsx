1d78b08a349c937ae0682fe49b9a9bbb19265043
mvazquez <matias.vazquezmartin@gmail.com>
2024-08-16 12:08:42 -0300

    fix: create cvData function

--- src/context/CVContext.jsx
+++ src/context/CVContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-
+import { v4 as uuidv4 } from 'uuid';
 export const CVContext = createContext();
 
 export const CVProvider = ({ children }) => {
@@ -40,10 +40,117 @@ export const CVProvider = ({ children }) => {
     localStorage.setItem('cvData', JSON.stringify(defaultData));
   };
 
+  const handleContactChange = (field, value) => {
+    setCvData((prevData) => ({
+      ...prevData,
+      contact: {
+        ...prevData.contact,
+        [field]: value,
+      },
+    }));
+  };
+
+  const addExperience = () => {
+    setCvData((prevData) => ({
+      ...prevData,
+      experiences: [
+        ...prevData.experiences,
+        {
+          id: uuidv4(),
+          company: t('cv_builder.defaultCompany'),
+          location: t('cv_builder.defaultLocation'),
+          position: t('cv_builder.defaultPosition'),
+          startDate: t('cv_builder.defaultStartDate'),
+          endDate: t('cv_builder.defaultEndDate'),
+          tasks: [t('cv_builder.defaultTask1'), t('cv_builder.defaultTask2')],
+        },
+      ],
+    }));
+  };
+
+  const removeExperience = (id) => {
+    setCvData((prevData) => ({
+      ...prevData,
+      experiences: prevData.experiences.filter(
+        (experience) => experience.id !== id
+      ),
+    }));
+  };
+
+  const addTask = (experienceIndex) => {
+    setCvData((prevData) => {
+      const newExperiences = [...prevData.experiences];
+      newExperiences[experienceIndex].tasks.push('- ');
+      return {
+        ...prevData,
+        experiences: newExperiences,
+      };
+    });
+  };
+
+  const removeTask = (experienceIndex, taskIndex) => {
+    setCvData((prevData) => {
+      const newExperiences = [...prevData.experiences];
+      newExperiences[experienceIndex].tasks = newExperiences[
+        experienceIndex
+      ].tasks.filter((task, index) => index !== taskIndex);
+      return {
+        ...prevData,
+        experiences: newExperiences,
+      };
+    });
+  };
+
+  const addEducation = () => {
+    setCvData((prevData) => ({
+      ...prevData,
+      education: [
+        ...prevData.education,
+        {
+          id: uuidv4(),
+          institution: t('cv_builder.defaultInstitution'),
+          location: t('cv_builder.defaultEducationLocation'),
+          title: t('cv_builder.defaultTitle'),
+          date: t('cv_builder.defaultDate'),
+        },
+      ],
+    }));
+  };
+
+  const removeEducation = (id) => {
+    setCvData((prevData) => ({
+      ...prevData,
+      education: prevData.education.filter((education) => education.id !== id),
+    }));
+  };
+
+  const addSkill = () => {
+    setCvData((prevData) => ({
+      ...prevData,
+      skills: [...prevData.skills, t('cv_builder.defaultSkill')],
+    }));
+  };
+
+  const removeSkill = (index) => {
+    setCvData((prevData) => ({
+      ...prevData,
+      skills: prevData.skills.filter((_, skillIndex) => skillIndex !== index),
+    }));
+  };
+
   const value = {
     cvData,
     setCvData,
     resetCvData,
+    handleContactChange,
+    addExperience,
+    removeExperience,
+    addTask,
+    removeTask,
+    addEducation,
+    removeEducation,
+    addSkill,
+    removeSkill,
     changeLanguage: i18n.changeLanguage,
   };
 
