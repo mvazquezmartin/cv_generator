77537b7ca683c45d42cc5ac6cc368c9dc2897457
mvazquez <matias.vazquezmartin@gmail.com>
2024-08-17 15:28:55 -0300

    feat: add scrollTop

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { MainLayout } from '@/pages/Home/MainLayout';
 import { CVBuilder } from '@/pages/CreateCV/CVBuilder';
-import PDFPreview from '@/pages/PreViewCV/PDFView';
+import { PDFPreview } from '@/pages/PreViewCV/PDFView';
 import { CVProvider } from '@/context/CVContext';
+import { ScrollToTop } from '@/utils/ScrollTop';
 import { Faq } from '@/pages/Faq/Faq';
 import '@/style/App.css';
-import ScrollToTop from '@/utils/ScrollTop';
 
 function App() {
   return (
